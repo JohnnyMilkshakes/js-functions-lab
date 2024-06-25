a114d8e80041f4cefe6d65fdc8162a38349efe40
Jonathan C <johnnymilkshakes@icloud.com>
2024-06-25 18:43:10 -0400

    Saving Progress

---
+++ script.js
@@ -0,0 +1,178 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+  
+  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+  
+  /*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+
+const isAdult = (age) => {
+    if (age >= 18) {
+        return "Adult"
+    } else {
+        return "Minor"
+    }
+}
+
+console.log('Exercise 2 Result:', isAdult(25));
+
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+const isCharAVowel = (char) => {
+    
+    const vowles = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"];
+    
+    for (i=0; i <= vowles.length; i++) {
+        if (char === vowles[i]) {
+            return true
+        }
+    }
+  return false
+}
+
+console.log('Exercise 3 Result:', isCharAVowel("z"));
+
+
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+const generateEmail = (name, domain) => {
+    return `${name}@${domain}`
+}
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+const greetUser = (name, time) => {
+    if (time === 'morning') {
+        return `Rise and Shine ${name}`
+    } else if (time === 'afternoon') {
+        return `Wen lunch ${name}?`
+    } else if (time === 'evening') {
+        return `Starting ${name}'s wind down time`
+    } else {
+        return `Bad news ${name}, you've either entered a time of day that isnt accounted for or you exist outside the limits of time, lets hope it's the former.`
+    }
+}
+
+console.log('Exercise 5 Result:', greetUser("Sam", "evening"));
+
+
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+const maxOfThree = (x, y, z) => {
+    // if (x > y) {
+    //     if (x > z ) {
+    //         return x;
+    //     }
+    //   } else if (y > z) {
+    //     return y;
+    //   } else {
+    //     return z;
+    //   }
+
+    if (x > y && x > z) {
+        return x;
+    } else if (y > x && y > z) {
+        return y;
+    } else {
+        return z;
+    }
+}
+
+console.log('Exercise 6 Result:', maxOfThree(100, 300, 47));
+
+
+
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+
+const calculateTip = (bill, tipPercent) => {
+    return bill * (tipPercent/100)
+}
+
+console.log('Exercise 7 Result:', calculateTip(50, 20));
+
+
+const g = () => {
+    
+}
